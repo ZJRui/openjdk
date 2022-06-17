6020039093edca3a88e2057d7814b3b7bb877c01
mac <mac.11com>
2022-06-17 13:22:09 +0800

    Instrumentation

--- src/java.instrument/share/classes/java/lang/instrument/Instrumentation.java
+++ src/java.instrument/share/classes/java/lang/instrument/Instrumentation.java
@@ -141,6 +141,10 @@ public interface Instrumentation {
      * after it has been removed. Transformers should be written defensively
      * to expect this situation.
      *
+     * 注销提供的变压器。以后的类定义将不会显示给转换器。删除最近添加的转换器匹配实例。
+     * 由于类装入的多线程性质，在删除转换器之后，它有可能接收调用。
+     * 变形金刚应该以防御的方式编写，以应对这种情况。
+     *
      * @param transformer          the transformer to unregister
      * @return  true if the transformer was found and removed, false if the
      *           transformer was not found
@@ -161,6 +165,14 @@ public interface Instrumentation {
      * this capability.
      * During a single instantiation of a single JVM, multiple calls to this
      * method will always return the same answer.
+     *
+     * <p>
+     *     返回当前JVM配置是否支持类的重新转换。重新转换已经加载的类的能力是JVM的可选功能。
+     *     只有当代理JAR文件中的Can-Retransform-Classes清单属性设置为true(如包规范中所述)并且JVM支持此功能时，
+     *     才支持重新转换。在单个JVM的单个实例化过程中，对该方法的多次调用将始终返回相同的答案。
+     * </p>
+     *
+     *
      * @return  true if the current JVM configuration supports retransformation of
      *          classes, false if not.
      * @see #retransformClasses
@@ -224,6 +236,7 @@ public interface Instrumentation {
      * The order of transformation is described in {@link ClassFileTransformer}.
      * This same order is used in the automatic reapplication of
      * retransformation incapable transforms.
+     * <p>转换的顺序在ClassFileTransformer中描述。同样的顺序被用于再变换无能力变换的自动再应用。</p>
      * <P>
      *
      * The initial class file bytes represent the bytes passed to
@@ -238,35 +251,54 @@ public interface Instrumentation {
      *  Some attributes may not be present.
      *  Where order is not meaningful, for example the order of methods,
      *  order might not be preserved.
+     *  <p>
+     *      初始的类文件字节表示传递给ClassLoader.defineClass或redefineclass的字节(在应用任何转换之前)，
+     *      但是它们可能与它们不完全匹配。常量池可能没有相同的布局或内容。常量池可能有更多或更少的条目。
+     *      常数池条目的顺序可能不同;但是，方法的字节码中的常量池索引将相对应。有些属性可能不存在。
+     *      如果顺序没有意义，例如方法的顺序，则可能不保留顺序。
+     *  </p>
      *
      * <P>
      * This method operates on
      * a set in order to allow interdependent changes to more than one class at the same time
      * (a retransformation of class A can require a retransformation of class B).
+     * <p>
+     *     该方法对一个集合进行操作，以便允许同时对多个类进行相互依赖的更改(类a的重新转换可能需要类B的重新转换)。
+     * </p>
      *
      * <P>
      * If a retransformed method has active stack frames, those active frames continue to
      * run the bytecodes of the original method.
      * The retransformed method will be used on new invokes.
+     * <p>
+     *     如果重新转换的方法具有活动的堆栈帧，那么这些活动帧将继续运行原始方法的字节码。转换后的方法将用于新的调用。
+     * </p>
      *
      * <P>
      * This method does not cause any initialization except that which would occur
      * under the customary JVM semantics. In other words, redefining a class
      * does not cause its initializers to be run. The values of static variables
      * will remain as they were prior to the call.
+     * <p>
+     *     除了在习惯的JVM语义下发生的初始化外，此方法不会导致任何初始化。
+     *     换句话说，重新定义一个类不会导致它的初始化式运行。静态变量的值将保持在调用之前的值。
+     * </p>
      *
      * <P>
      * Instances of the retransformed class are not affected.
-     *
+     *重新转换的类的实例不受影响。
      * <P>
      * The supported class file changes are described in
      * <a href="{@docRoot}/../specs/jvmti.html#RetransformClasses">JVM TI RetransformClasses</a>.
      * The class file bytes are not checked, verified and installed
      * until after the transformations have been applied, if the resultant bytes are in
      * error this method will throw an exception.
-     *
+     *<p>
+     *     支持的类文件更改在JVM TI RetransformClasses中描述。在应用转换之前，不会检查、验证和安装类文件字节，如果结果字节出错，此方法将抛出异常。
+     *</p>
      * <P>
      * If this method throws an exception, no classes have been retransformed.
+     * 如果此方法引发异常，则没有重新转换任何类
      * <P>
      * This method is intended for use in instrumentation, as described in the
      * {@linkplain Instrumentation class specification}.
@@ -306,6 +338,11 @@ public interface Instrumentation {
      * this capability.
      * During a single instantiation of a single JVM, multiple calls to this
      * method will always return the same answer.
+     * <p>
+     *     返回当前JVM配置是否支持类的重定义。重新定义已经加载的类的能力是JVM的一个可选功能。
+     *     只有当代理JAR文件中的Can-Redefine-Classes清单属性设置为true(如包规范中所述)并且JVM支持此功能时，
+     *     才支持重定义。在单个JVM的单个实例化过程中，对该方法的多次调用将始终返回相同的答案。
+     * </p>
      * @return  true if the current JVM configuration supports redefinition of classes,
      * false if not.
      * @see #redefineClasses
@@ -315,6 +352,9 @@ public interface Instrumentation {
 
     /**
      * Redefine the supplied set of classes using the supplied class files.
+     *<p>
+     *     使用提供的类文件重新定义提供的类集。
+     *</p>
      *
      * <P>
      * This method is used to replace the definition of a class without reference
@@ -324,25 +364,35 @@ public interface Instrumentation {
      * example in bytecode instrumentation)
      * {@link #retransformClasses retransformClasses}
      * should be used.
+     * <p>
+     *     此方法用于在不引用现有类文件字节的情况下替换类的定义，就像从源代码重新编译以进行修复并继续调试时所做的那样。
+     *     如果要转换现有的类文件字节(例如在字节码插装中)，应该使用retransformClasses。
+     * </p>
      *
      * <P>
      * This method operates on
      * a set in order to allow interdependent changes to more than one class at the same time
      * (a redefinition of class A can require a redefinition of class B).
-     *
+     *<p>该方法操作一个集合，以便允许同时对多个类进行相互依赖的更改(重新定义类a可能需要重新定义类B)。</p>
      * <P>
      * If a redefined method has active stack frames, those active frames continue to
      * run the bytecodes of the original method.
      * The redefined method will be used on new invokes.
+     * <p>如果重定义的方法具有活动的堆栈帧，那么这些活动帧将继续运行原始方法的字节码。重新定义的方法将在新的调用中使用。</p>
      *
      * <P>
      * This method does not cause any initialization except that which would occur
      * under the customary JVM semantics. In other words, redefining a class
      * does not cause its initializers to be run. The values of static variables
      * will remain as they were prior to the call.
+     * <p>
+     *     除了在习惯的JVM语义下发生的初始化外，此方法不会导致任何初始化。换句话说，重新定义一个类不会导致它的初始化式运行。
+     *     静态变量的值将保持在调用之前的值。
+     * </p>
      *
      * <P>
      * Instances of the redefined class are not affected.
+     * 重定义的类的实例不受影响。
      *
      * <P>
      * The supported class file changes are described in
@@ -351,11 +401,18 @@ public interface Instrumentation {
      * until after the transformations have been applied, if the resultant bytes are in
      * error this method will throw an exception.
      *
+     * <p>
+     *     支持的类文件更改在JVM TI redefineclass中描述。在应用转换之前，不会检查、验证和安装类文件字节，如果结果字节出错，此方法将抛出异常。
+     * </p>
+     *
      * <P>
      * If this method throws an exception, no classes have been redefined.
      * <P>
      * This method is intended for use in instrumentation, as described in the
      * {@linkplain Instrumentation class specification}.
+     * 如果此方法引发异常，则没有重新定义任何类。
+     * 如类规范中所述，此方法用于插装。
+     *
      *
      * @param definitions array of classes to redefine with corresponding definitions;
      *                    a zero-length array is allowed, in this case, this method does nothing
@@ -399,6 +456,11 @@ public interface Instrumentation {
      * Primitive classes (for example, <code>java.lang.Integer.TYPE</code>)
      * and array classes are never modifiable.
      *
+     * 测试类是否可以通过重新转换或重新定义修改。如果一个类是可修改的，那么这个方法返回true。如果一个类是不可修改的，那么这个方法返回false。
+     * 对于要重新转换的类，isRetransformClassesSupported也必须为真。但是isRetransformClassesSupported()的值不影响此函数返回的值。
+     * 对于要重新定义的类，isRedefineClassesSupported也必须为真。但是isRedefineClassesSupported()的值不影响此函数返回的值。
+     * 基元类(例如java.lang.Integer.TYPE)和数组类永远不可修改。
+     *
      * @param theClass the class to check for being modifiable
      * @return whether or not the argument class is modifiable
      * @throws java.lang.NullPointerException if the specified class is <code>null</code>.
@@ -438,6 +500,12 @@ public interface Instrumentation {
      * {@linkplain Class#isHidden() hidden class or interface}.
      * as they cannot be discovered by any class loader.
      *
+     * <p>
+     *     通过ClassLoader::loadClass, Class::forName和字节码链接返回所有类的数组。
+     *     也就是说，装入器被记录为初始化装入器的所有类。如果提供的加载器为空，则返回引导程序类加载器可以通过名称找到的类。
+     * 返回的数组不包括隐藏类或接口，也不包括元素类型为隐藏类或接口的数组类。因为它们不能被任何类装入器发现。
+     * </p>
+     *
      * @param loader          the loader whose initiated class list will be returned
      * @return an array containing all classes which {@code loader} can find by name;
      *          zero-length if there are none
@@ -452,6 +520,10 @@ public interface Instrumentation {
      * and thus is useful for comparison within an implementation but not between implementations.
      *
      * The estimate may change during a single invocation of the JVM.
+     * <p>
+     *     返回指定对象所消耗的存储量的特定于实现的近似值。结果可能包括对象的部分或全部开销，
+     *     因此对于实现内部的比较很有用，但对于实现之间的比较则不行。在单次调用JVM期间，估计可能会发生变化。
+     * </p>
      *
      * @param objectToSize     the object to size
      * @return an implementation-specific approximation of the amount of storage consumed by the specified object
@@ -719,16 +791,29 @@ public interface Instrumentation {
      * that are read, the packages that are exported or open, or the services
      * that are used or provided.
      *
+     * <p>
+     *     重新定义一个模块，以扩展它读取的模块集、导出或打开的包集，或者它使用或提供的服务。
+     *     此方法方便了命名模块中的代码插装，其中插装需要更改读取的模块集、导出或打开的包，或使用或提供的服务。
+     * </p>
+     *
      * <p> This method cannot reduce the set of modules that a module reads, nor
      * reduce the set of packages that it exports or opens, nor reduce the set
      * of services that it uses or provides. This method is a no-op when invoked
      * to redefine an unnamed module. </p>
+     * <p>此方法不能减少模块读取的模块集，也不能减少模块导出或打开的包集，
+     * 也不能减少模块使用或提供的服务集。当调用此方法来重新定义未命名的模块时，此方法是无操作的。
+     * </p>
      *
      * <p> When expanding the services that a module uses or provides then the
      * onus is on the agent to ensure that the service type will be accessible at
      * each instrumentation site where the service type is used. This method
      * does not check if the service type is a member of the module or in a
      * package exported to the module by another module that it reads. </p>
+     *
+     * <p>
+     *     当扩展模块使用或提供的服务时，代理负责确保在使用该服务类型的每个检测站点上都可以访问该服务类型。
+     *     此方法不检查服务类型是模块的成员，还是在它读取的另一个模块导出到该模块的包中。
+     * </p>
      *
      * <p> The {@code extraExports} parameter is the map of additional packages
      * to export. The {@code extraOpens} parameter is the map of additional
@@ -737,6 +822,10 @@ public interface Instrumentation {
      * <cite>The Java Language Specification </cite>, for example, {@code
      * "java.lang"}. The map value is the non-empty set of modules that the
      * package should be exported or opened to. </p>
+     * <p>
+     *     extraExports参数是要导出的其他包的映射。extraopening参数是要打开的其他包的映射。在这两种情况下，map键都是在Ja
+     *     va语言规范6.5.3节中定义的包的完全限定名，例如“Java .lang”。map值是包应该导出或打开到的非空模块集。
+     * </p>
      *
      * <p> The {@code extraProvides} parameter is the additional service providers
      * for the module to provide. The map key is the service type. The map value
